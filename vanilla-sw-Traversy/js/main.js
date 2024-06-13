c82a4caf1fb9763fe3ef0be7675f100e08313226
Lakshani Hewage <madhuwanthi.hewage@gmail.com>
2024-06-13 11:39:42 +0530

    change file name

--- vanilla-sw-Traversy/js/main.js
+++ vanilla-sw-Traversy/js/main.js
@@ -2,7 +2,7 @@
 if('serviceWorker' in navigator){
     window.addEventListener('load', () => {
         navigator.serviceWorker
-        .register('../sw_cached_pages.js')
+        .register('../sw_cached_site.js')
         .then(reg => console.log('service worker registered',reg))
         .catch(err => console.log('service worker not registered', err))
     })
